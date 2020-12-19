43493c453e7b57d2ed9fd75a4c9bc3d5c9f319ca
Nina Velikajne <nv6920@student.uni-lj.si>
2020-12-19 19:32:15 +0100

    Docs files

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'RhythmCount',
+  packages = ['RhythmCount'],
+  version = '1.1',
+  license='MIT',
+  description = 'Python package for cosinor based rhytmometry in count data',
+  author = 'Nina Velikajne',
+  author_email = 'nina.velikajne@gmail.com',
+  url = 'https://github.com/ninavelikajne/RhythmCount',
+  download_url = 'https://github.com/ninavelikajne/RhythmCount/archive/v1.1.tar.gz',
+  keywords = ['cosinor', 'rhytmometry', 'regression', 'count data'],
+  install_requires=[
+          'pandas',
+          'numpy',
+          'matplotlib',
+          'statsmodels',
+          'scipy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
